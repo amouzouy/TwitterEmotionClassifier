a6bf12a964baf415295ec986b354053de2b3ecb4
Sheryan Resutov <sheryan.resutov@yodle.com>
2016-03-26 13:00:11 -0400

    Fixing scaling

--- runner.sh
+++ runner.sh
@@ -15,8 +15,13 @@ modelPath="$path/dataset/models"
 outputPath="$path/dataset/outputs"
 IFS=","
 gradle jar
-while read f1 f2 f3 f4
+while read f1 f2 f3 f4 f5
 do
+        if [[ "$f5" == "yes" ]]; then
+                scaling="_scaled"
+        else
+                scaling=""
+        fi
         [ "$f1" = "Description" ] && continue
         svmParams=()
         prepParams=()
@@ -26,11 +31,13 @@ do
         END=$(expr ${f4} - 1)
         for ((i=0;i<=END;i++));
                 do
-                        ${LibSVMPath}/svm-scale$ext ${trainPath}/file$i.train > ${trainPath}/file${i}_scaled.train
-                        ${LibSVMPath}/svm-scale$ext ${testPath}/file$i.test > ${testPath}/file${i}_scaled.test
-                        ${LibSVMPath}/svm-train$ext ${svmParams[@]} ${trainPath}/file${i}_scaled.train
-                        mv ./file${i}_scaled.train.model ${modelPath}
-                        ${LibSVMPath}/svm-predict$ext ${testPath}/file${i}_scaled.test ${modelPath}/file${i}_scaled.train.model ${outputPath}/file$i.output
+                        if [[ "$f5" == "yes" ]]; then
+                                ${LibSVMPath}/svm-scale$ext ${trainPath}/file$i.train > ${trainPath}/file${i}_scaled.train
+                                ${LibSVMPath}/svm-scale$ext ${testPath}/file$i.test > ${testPath}/file${i}_scaled.test
+                        fi
+                        ${LibSVMPath}/svm-train$ext ${svmParams[@]} ${trainPath}/file${i}${scaling}.train
+                        mv ./file${i}${scaling}.train.model ${modelPath}
+                        ${LibSVMPath}/svm-predict$ext ${testPath}/file${i}${scaling}.test ${modelPath}/file${i}${scaling}.train.model ${outputPath}/file$i.output
                 done
                 java -cp ${jarPath}/emotionclassifier-1.0-SNAPSHOT.jar ResultsGenerator $f4 "$f2" "$f3" "$f1"
 				if [[ "$OSTYPE" == "linux-gnu" ]]; then
